5e71a8bac3b3c0ddf00ebaf96bb9251e70bfd3d8
Greg McIntyre <gregmcintyre1984@gmail.com>
2019-07-08 13:45:09 +1000

    ADD: 404.html

---
+++ README.md
@@ -0,0 +1,7 @@
+#TrackMe
+##Greg McIntyre
+SIT209 - Software Engineering 2: Developing Internet-of-things Applications
+
+run from /web with node server.js or npm start
+
+output at http://localhost:3000/
--- web/server.js
+++ web/server.js
@@ -10,11 +10,27 @@ const base = `${__dirname}/public`;
 app.use(express.static('public'));
 
 //route middleware for the root URI path
-app.get('/', (req, res) => {
-	res.send('hello world');
+app.get('/', function (req, res) {
+	res.sendFile(`${base}/device-list.html`);
+});
+
+app.get('/register-device', (req, res) => {
+	res.sendFile(`${base}/register-device.html`);
+});
+
+app.get('/send-command', (req, res) => {
+	res.sendFile(`${base}/send-command.html`);
+});
+
+app.get('/about', (req, res) => {
+	res.sendFile(`${base}/about-me.html`);
 });
 
 //start the web server and listen to requests on the specified port
 app.listen(port, () => {
 	console.log(`listening on port ${port}`);
+});
+
+app.get('*', (req, res) => {
+	res.sendFile(`${base}/404.html`);
 });
