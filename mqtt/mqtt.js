53055423c259f07a85d1b4d30b9ef7c2fd2de681
Greg McIntyre <gregmcintyre1984@gmail.com>
2019-08-15 13:28:58 +1000

    ADD: MQTT API initial

--- mqtt/mqtt.js
+++ mqtt/mqtt.js
@@ -1,12 +1,27 @@
+//require('dotenv').config();
 const mqtt = require('mqtt');
 
 const { URL, USERNAME, PASSWORD } = process.env;
 
-const client = mqtt.connect(URL, {
+/*const client = mqtt.connect(URL, {
 	username: USERNAME,
 	password: PASSWORD
+});*/
+
+const client = mqtt.connect({
+	host: 'soldier.cloudmqtt.com:17826',
+	username: 'bzwlzhhd',
+	password: 'U1q2RGhiLof6'
 });
+
+console.log(client);
 
 client.on('connect', () => {
 	console.log('connected');
+	
+	const topic = '/test/hello/';
+	const msg = 'Hello MQTT world!';
+	client.publish(topic, msg, () => {
+		console.log('message sent...');
+	});	
 });
